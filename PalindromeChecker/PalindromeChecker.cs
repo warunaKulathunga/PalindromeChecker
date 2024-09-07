a9b7902e7fd54b5851457f1e88800210a2a69d77
DESKTOP-VVL8G02\Waruna <waruna532@gmail.com>
2024-09-07 10:50:28 +0530

    Add project files.

---
+++ PalindromeChecker/PalindromeChecker.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace PalindromeCheckerApp
+{
+    public class PalindromeChecker
+    { 
+        public bool IsPalindrome(string inputString, string trashSymbols)
+        {
+            
+            int left = 0;
+            int right = inputString.Length - 1;
+
+            HashSet<char> trashSet = new HashSet<char>(trashSymbols);
+
+            while (left < right)
+            {
+                
+                while (left < right && trashSet.Contains(inputString[left]))
+                {
+                    left++;
+                }
+
+                while (left < right && trashSet.Contains(inputString[right]))
+                {
+                    right--;
+                }
+
+                if (char.ToLower(inputString[left]) != char.ToLower(inputString[right]))
+                {
+                    return false;
+                }
+
+                left++;
+                right--;
+            }
+            
+            return true;
+        }
+    }
+}
---
+++ PalindromeChecker/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace PalindromeCheckerApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            PalindromeChecker checker = new PalindromeChecker();
+
+            string inputString1 = "a@b!!b$a";
+            string trashSymbols1 = "!@$";
+            bool result1 = checker.IsPalindrome(inputString1, trashSymbols1);
+            Console.WriteLine($"Is \"{inputString1}\" a palindrome? {result1}");
+
+            string inputString2 = "?Aa#c";
+            string trashSymbols2 = "#?";
+            bool result2 = checker.IsPalindrome(inputString2, trashSymbols2);
+            Console.WriteLine($"Is \"{inputString2}\" a palindrome? {result2}");
+
+            Console.WriteLine("Enter Input String:");
+            string inputString = Console.ReadLine();
+
+            Console.WriteLine("Enter Trash Symbols:");
+            string trashSymbols = Console.ReadLine();
+
+            bool result = checker.IsPalindrome(inputString, trashSymbols);
+            Console.WriteLine($"Is \"{inputString}\" a palindrome? {result}");
+
+        }
+    }
+}
